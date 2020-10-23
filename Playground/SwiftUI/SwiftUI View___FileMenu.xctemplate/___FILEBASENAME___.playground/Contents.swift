a69d3d1b5311828aae1862bd42352e45c037e236
Danila Shikulin <danskeel@gmail.com>
2020-10-23 23:45:41 +0300

    Added playground templates for SwiftUI View

---
+++ Playground/SwiftUI/SwiftUI View___FileMenu.xctemplate/___FILEBASENAME___.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: A SwiftUI based Playground for presenting user interface
+  
+import PlaygroundSupport
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+PlaygroundPage.current.setLiveView(ContentView())
